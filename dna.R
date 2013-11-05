ee57737fedebf4f1f1b12825eb85bf65110e512b
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2013-11-05 12:22:53 +0000

    Updated gene info and made gene download function
    git-svn-id: https://microb98.med.upenn.edu/svn/scott/scripts/R@7754 acd2042c-187b-11de-8279-d8d34a11d372

--- dna.R
+++ dna.R
@@ -305,20 +305,24 @@ dna2codons<-function(dna,frame=0){
 #convert codon to amino acid (using aminoAcids table below)
 #codons: vector of 3 base codons
 #type: amino acid info to return; code for single letter, name for full name, or abbr for 3-letter abbreviation
-codon2aa<-function(codons,type='code'){
+#naReplace: replace unknown codons with
+codon2aa<-function(codons,type='code',naReplace='z',warn=TRUE){
 	if(!type %in% c('code','name','abbr'))stop(simpleError('Invalid amino acid type'))
 	codons<-gsub('T','U',toupper(codons))
-	return(aminoAcids[,type,drop=FALSE][codons,1])
+	aas<-aminoAcids[,type,drop=FALSE][codons,1]
+	if(warn&any(is.na(aas)))warning('Unknown codons')
+	aas[is.na(aas)]<-naReplace
+	return(aas)
 }
 
 #convert dna/rna to amino acids
 #dna: a string of DNA/RNA
 #frame: starting frame (0=start on first base, 1=on second, 2=on third)
 #debug: print debug info?
-dna2aa<-function(dna,frame=0,debug=FALSE){
+dna2aa<-function(dna,frame=0,debug=FALSE,...){
 	codons<-dna2codons(dna,frame)	
 	if(debug)print(codons)
-	output<-paste(codon2aa(codons),collapse='')
+	output<-paste(codon2aa(codons,...),collapse='')
 	return(output)
 }
 
@@ -2619,3 +2623,28 @@ samFlags$bit<-2^(0:(nrow(samFlags)-1))
 rownames(samFlags)<-samFlags$short
 #data.frame of amino acids
 aminoAcids<-data.frame('codon'=c('UUU','UUC','UCU','UCC','UAU','UAC','UGU','UGC','UUA','UCA','UAA','UGA','UUG','UCG','UAG','UGG','CUU','CUC','CCU','CCC','CAU','CAC','CGU','CGC','CUA','CUG','CCA','CCG','CAA','CAG','CGA','CGG','AUU','AUC','ACU','ACC','AAU','AAC','AGU','AGC','AUA','ACA','AAA','AGA','AUG','ACG','AAG','AGG','GUU','GUC','GCU','GCC','GAU','GAC','GGU','GGC','GUA','GUG','GCA','GCG','GAA','GAG','GGA','GGG'),'abbr'=c('Phe','Phe','Ser','Ser','Tyr','Tyr','Cys','Cys','Leu','Ser','Ochre','Opal','Leu','Ser','Amber','Trp','Leu','Leu','Pro','Pro','His','His','Arg','Arg','Leu','Leu','Pro','Pro','Gln','Gln','Arg','Arg','Ile','Ile','Thr','Thr','Asn','Asn','Ser','Ser','Ile','Thr','Lys','Arg','Met','Thr','Lys','Arg','Val','Val','Ala','Ala','Asp','Asp','Gly','Gly','Val','Val','Ala','Ala','Glu','Glu','Gly','Gly'),'code'=c('F','F','S','S','Y','Y','C','C','L','S','X','X','L','S','X','W','L','L','P','P','H','H','R','R','L','L','P','P','Q','Q','R','R','I','I','T','T','N','N','S','S','I','T','K','R','M','T','K','R','V','V','A','A','D','D','G','G','V','V','A','A','E','E','G','G'),'name'=c('Phenylalanine','Phenylalanine','Serine','Serine','Tyrosine','Tyrosine','Cysteine','Cysteine','Leucine','Serine','Stop','Stop','Leucine','Serine','Stop','Tryptophan','Leucine','Leucine','Proline','Proline','Histidine','Histidine','Arginine','Arginine','Leucine','Leucine','Proline','Proline','Glutamine','Glutamine','Arginine','Arginine','Isoleucine','Isoleucine','Threonine','Threonine','Asparagine','Asparagine','Serine','Serine','Isoleucine','Threonine','Lysine','Arginine','Methionine','Threonine','Lysine','Arginine','Valine','Valine','Alanine','Alanine','Aspartic acid','Aspartic acid','Glycine','Glycine','Valine','Valine','Alanine','Alanine','Glutamic acid','Glutamic acid','Glycine','Glycine'),row.names=c('UUU','UUC','UCU','UCC','UAU','UAC','UGU','UGC','UUA','UCA','UAA','UGA','UUG','UCG','UAG','UGG','CUU','CUC','CCU','CCC','CAU','CAC','CGU','CGC','CUA','CUG','CCA','CCG','CAA','CAG','CGA','CGG','AUU','AUC','ACU','ACC','AAU','AAC','AGU','AGC','AUA','ACA','AAA','AGA','AUG','ACG','AAG','AGG','GUU','GUC','GCU','GCC','GAU','GAC','GGU','GGC','GUA','GUG','GCA','GCG','GAA','GAG','GGA','GGG'),stringsAsFactors=FALSE)
+
+
+
+#http://life.nthu.edu.tw/~fmhsu/rasframe/SHAPELY.HTM
+aminoColors<-data.frame('aa'=c("ASP","GLU","CYS","MET","LYS","ARG","SER","THR","PHE","TYR","ASN","GLN","GLY","LEU","VAL","ILE","ALA","TRP","HIS","PRO"),'col'=c("#E60A0A","#E60A0A","#E6E600","#E6E600","#145AFF","#145AFF","#FA9600","#FA9600","#3232AA","#3232AA","#00DCDC","#00DCDC","#EBEBEB","#0F820F","#0F820F","#0F820F","#C8C8C8","#B45AB4","#8282D2","#DC9682"),stringsAsFactors=FALSE)
+tmp<-with(aminoAcids[!duplicated(aminoAcids[,c('code')]),],data.frame('letter'=code,row.names=toupper(abbr),stringsAsFactors=FALSE))
+rownames(aminoColors)<-tmp[aminoColors$aa,'letter']
+tmpAngles1<-cos(1+1:nrow(aminoColors)/nrow(aminoColors)*pi)
+tmpAngles2<-sin(1:nrow(aminoColors)/nrow(aminoColors)*pi)
+tmpAngles1<-tapply(tmpAngles1,aminoColors$col,c)
+tmpAngles2<-tapply(tmpAngles2,aminoColors$col,c)
+aminoColors$spreadCol<-ave(aminoColors$col,aminoColors$col,FUN=function(x){
+	if(length(x)==1)return(x)
+	spacer<-20
+	angles1<-tmpAngles1[[x[1]]]
+	angles2<-tmpAngles2[[x[1]]]
+	spacing<-seq((length(x)-1)*-spacer,(length(x)-1)*spacer,length.out=length(x))
+	lab<-convertColor(t(col2rgb(x[1])),from='sRGB',to='Lab',scale.in=255)[rep(1,length(x)),]
+	lab[,'b']<-lab[,'b']-spacing*angles1
+	lab[,'a.x']<-lab[,'a.x']+spacing*angles2
+	rgbs<-convertColor(lab,from='Lab',to='sRGB')
+	return(rgb(rgbs))
+	#plot(1:nrow(aminoColors),col=aminoColors$col,lwd=30,xaxt='n');points(1:nrow(aminoColors),1:nrow(aminoColors)+1,col=aminoColors$spreadCol,lwd=20);axis(1,1:nrow(aminoColors),aminoColors$aa,las=2)
+})
+rm(tmpAngles1,tmpAngles2)
