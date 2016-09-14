5a797a2270ae9f7b34feab28c94013956715153f
sherrillmix <sherrillmix@users.noreply.github.com>
2016-09-14 14:20:32 -0400

    Reverse argument order of withAs

--- R/misc.R
+++ R/misc.R
@@ -519,19 +519,25 @@ fillDown<-function(x,emptyStrings=c(NA,''),errorIfFirstEmpty=TRUE){
 #'
 #' An alternative to the \code{with} function in base R where \code{data} is represented by a temporary variable within a new environment rather than evaluating \code{expr} directly within \code{data}. This can help explicitly show which variables are contained within \code{data} from those outside.
 #'
-#' @param expr expression to evaluate.
 #' @param ... data to be used in expression. Argument names are used as temporary variable names. So \code{xx=data.frame('a'=1:10)} will result in a data.frame called \code{xx} with column \code{a} in the evaluating environment.
+#' @param expr expression to evaluate. if expr is not defined by name then the last unnamed argument is used.
 #' @export
 #' @return the value of the evaluated \code{expr}
 #' @examples
 #' d<-3:12
 #' longNameDataFrame<-data.frame('a'=1:10,'b'=2:11)
 #' with(longNameDataFrame,a+b+d)
-#' withAs(xx$a+xx$b+d,xx=longNameDataFrame)
-withAs<-function(expr,...){
+#' withAs(xx=longNameDataFrame,xx$a+xx$b+d)
+#' anotherDf<-data.frame('c'=2:12)
+#' withAs(xx=longNameDataFrame,zz=anotherDf,xx$a+xx$b+zz$c)
+withAs<-function(...,expr=NULL){
   parent<-parent.frame()
   env<-new.env(parent=parent)
   dotVars<-match.call(expand.dots=FALSE)$'...'
+  if(missing(expr)){
+    expr<-dotVars[[length(dotVars)]]
+    dotVars<-dotVars[-length(dotVars)]
+  }
   if(is.null(names(dotVars))||any(names(dotVars)==''))stop(simpleError('Unassigned variables passed to withAs'))
   #make sure to get parent above and not within the mapply
   mapply(function(as,val)assign(as,eval(val,envir=parent),env),names(dotVars),dotVars)
