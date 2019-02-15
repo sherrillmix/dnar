0322bc601cbd0d51c09f97ca168c2284545340c2
sherrillmix <sherrillmix@users.noreply.github.com>
2019-02-15 05:48:27 -0500

    Add cex arg to insetScale

--- R/misc.R
+++ R/misc.R
@@ -656,6 +656,7 @@ logAxis<-function(side=2,exponent=TRUE,addExtra=!exponent,minorTcl=-.2,axisMin=-
 #' @param at positions for labels on the axis
 #' @param labels labels for the axis
 #' @param offset add a small offset to rectangles to suppress pdf viewers showing a gap at exact border between rectangles
+#' @param cex character size for axis and labels
 #' @export
 #' @seealso \code{\link[stats]{heatmap}}, \code{\link[graphics]{image}}
 #' @return NULL
@@ -665,7 +666,7 @@ logAxis<-function(side=2,exponent=TRUE,addExtra=!exponent,minorTcl=-.2,axisMin=-
 #' cols<-rev(heat.colors(length(breaks)-1))
 #' heatmap(dists,col=cols)
 #' insetScale(breaks,cols,main='Distance')
-insetScale<-function(breaks,col,insetPos=c(.025,.015,.04,.25),main='',offset=1e-3,at=NULL,labels=NULL){
+insetScale<-function(breaks,col,insetPos=c(.025,.015,.04,.25),main='',offset=1e-3,at=NULL,labels=NULL,cex=1){
   if(length(breaks)!=length(col)+1)stop('Number of breaks must be one more than colors')
   insetPos<-c(graphics::grconvertY(insetPos[1],'nfc','user'),graphics::grconvertX(insetPos[2],'nfc','user'),graphics::grconvertY(insetPos[3],'nfc','user'),graphics::grconvertX(insetPos[4],'nfc','user'))
   breakPos<-((breaks)-(min(breaks)))/max((breaks)-(min(breaks)))*(insetPos[4]-insetPos[2])+insetPos[2]
@@ -680,8 +681,8 @@ insetScale<-function(breaks,col,insetPos=c(.025,.015,.04,.25),main='',offset=1e-
   if(is.null(labels))labels<-at
   convertPos<-(at-(min(breaks)))/((max(breaks))-(min(breaks)))*(insetPos[4]-insetPos[2])+insetPos[2]
   graphics::segments(convertPos,insetPos[1],convertPos,insetPos[1]-diff(insetPos[c(1,3)])*.1,xpd=NA)
-  graphics::text(convertPos,insetPos[1]-diff(insetPos[c(1,3)])*.175,labels,xpd=NA,adj=c(.5,1),cex=.85)
-  graphics::text(mean(insetPos[c(2,4)]),insetPos[3]+diff(insetPos[c(1,3)])*.45,main,xpd=NA,adj=c(.5,0))
+  graphics::text(convertPos,insetPos[1]-diff(insetPos[c(1,3)])*.175,labels,xpd=NA,adj=c(.5,1),cex=.85*cex)
+  graphics::text(mean(insetPos[c(2,4)]),insetPos[3]+diff(insetPos[c(1,3)])*.45,main,xpd=NA,adj=c(.5,0),cex=cex)
   invisible(NULL)
 }
 
